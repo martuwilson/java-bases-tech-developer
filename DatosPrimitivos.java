5e35beb17f623afa7aca5e1f06c8bd468fd3a82d
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-10-08 20:27:40 -0300

    Datos primitivos vs objetos

---
+++ DatosPrimitivos.java
@@ -0,0 +1,12 @@
+public class DatosPrimitivos {
+    public static void main(String[] args){
+        int number = 0; // primitive data type
+
+        Integer number2 = 0; // object data type
+
+        // diff entre datos primitivos y objetos
+        // datos primitivos no tienen metodos
+        // objetos tienen metodos como por ejemplo:
+        // number2.toString() -> hace que el numero se convierta en un string
+    }
+}
